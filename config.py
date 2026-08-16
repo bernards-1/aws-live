d26737989a739f8511da35d95bdd1bfb870096ab
bernards-1 <seikping1213@gmail.com>
2026-08-16 16:24:18 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cokxjyfpq1ih.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "12345678"
+customdb = "employee"
+custombucket = "lowchoonkeat-employee"
+customregion = "us-east-1"
